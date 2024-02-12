f30dee3d449a41f76d5e35d1728c28a2c3c7f408
emilynomura1 <49768143+emilynomura1@users.noreply.github.com>
2024-02-11 18:44:44 -1000

    Parse and clean check-in data

---
+++ data-prep/clean_checkin.py
@@ -0,0 +1,14 @@
+# Load packages
+import pandas as pd
+
+# Read in data
+file_path = "../data/check_in.html"
+check_in = pd.read_html(file_path)
+check_in_df = check_in[0]
+
+# Clean data
+check_in_df["Date"] = pd.to_datetime(check_in_df["Date"])
+check_in_df = check_in_df.drop(columns=['Comment', 'Status'])
+
+# Save new data
+check_in_df.to_pickle("../data/check_in.pkl")

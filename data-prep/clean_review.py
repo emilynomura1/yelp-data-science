9b5d7736a676a25af9e86308c0eb7e542a21fd46
emilynomura1 <49768143+emilynomura1@users.noreply.github.com>
2024-02-17 18:06:42 -1000

    Parse and clean reviews data

---
+++ data-prep/clean_review.py
@@ -0,0 +1,14 @@
+# Load packages
+import pandas as pd
+
+# Read in data
+file_path = "../data/user_review.html"
+reviews = pd.read_html(file_path)
+reviews_df = reviews[0]
+
+# Clean data
+reviews_df["Date"] = pd.to_datetime(reviews_df["Date"])
+reviews_df = reviews_df.drop(columns=['IP Address','Status'])
+
+# Save new data
+reviews_df.to_pickle("../data/user_review.pkl")

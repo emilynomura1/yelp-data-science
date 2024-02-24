d86f0b12e798502271dfe5f8bc9c2239e6d4bdb2
emilynomura1 <49768143+emilynomura1@users.noreply.github.com>
2024-02-24 12:19:47 -1000

    Change comments

--- data-prep/geocode_biz.py
+++ data-prep/geocode_biz.py
@@ -1,13 +1,12 @@
 import pandas as pd
 from geopy.geocoders import Nominatim
 
-# Uses current IP address to aid in geocoding addresses
 location = Nominatim(timeout=20, user_agent="yelp-ds-project")
 reviews = pd.read_pickle("../data/user_review.pkl")
 
 reviews["Geocode"] = reviews["Business Name"].apply(location.geocode)
 
-# Remove missing geocode entries to avoid error when extracting coordinates
+# Remove missing geocode entries
 reviews.dropna(inplace=True)
 
 reviews['Latitude'] = [g.latitude for g in reviews["Geocode"]]

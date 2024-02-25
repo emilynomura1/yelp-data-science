832caf08a1a42452ab0b100e9e43f414dff8db07
emilynomura1 <49768143+emilynomura1@users.noreply.github.com>
2024-02-24 22:43:35 -1000

    Generate wordcloud for searches

--- generate-figs/get_wordcloud.py
+++ generate-figs/get_wordcloud.py
@@ -1,25 +1,18 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud, ImageColorGenerator
-from PIL import Image
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-
-# Generate word cloud from a review
-stop_words = set(stopwords.words('english'))
-def get_word_cloud(review):
-    word_tokens = word_tokenize(review)
-    filtered_sentence = [word.lower() for word in word_tokens if word not in stop_words]
-    sentence = ' '.join(filtered_sentence)
-    wordcloud = WordCloud(width=500, 
-                          height=300, 
-                          max_words=50, 
-                          stopwords=stop_words, 
-                          colormap='Blues').generate_from_text(sentence)
-    plt.imshow(wordcloud)
-    plt.axis('off')
-    plt.savefig('../figures/wordcloud.png', bbox_inches='tight')
+from wordcloud_func import get_wordcloud
 
 reviews = pd.read_pickle("../data/user_review.pkl")
-get_word_cloud(reviews["Comment"][100])
+search = pd.read_pickle("../data/search.pkl")
+
+# Clean search data
+search_list = []
+for i in search["Search Text"]:
+    if pd.isna(i) == False:
+        search_list.append(i)
+search_string = ' '.join(search_list)
+
+# Get wordcloud for a review
+get_wordcloud(reviews["Comment"][100], "review")
+
+# Get wordcloud for searches
+get_wordcloud(search_string, "searches")
---
+++ generate-figs/wordcloud_func.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud, ImageColorGenerator
+from PIL import Image
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+
+# Generate word cloud from a review
+stop_words = set(stopwords.words('english'))
+def get_wordcloud(review, fig_name):
+    word_tokens = word_tokenize(review)
+    filtered_sentence = [word.lower() for word in word_tokens if word not in stop_words]
+    sentence = ' '.join(filtered_sentence)
+    wordcloud = WordCloud(width=500, 
+                          height=300, 
+                          max_words=50, 
+                          stopwords=stop_words, 
+                          colormap='Blues').generate_from_text(sentence)
+    fig_path = '../figures/' + fig_name + '_wordcloud.png'
+    plt.imshow(wordcloud)
+    plt.axis('off')
+    plt.savefig(fig_path, bbox_inches='tight')
